520982b42b43f4c726a5c6809f8790d6c160e65f
Jourdelune <jourdelune863@gmail.com>
2023-01-02 13:51:42 +0100

    cli usage

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 
 import aiofiles.os
+import tempfile
 import aiofiles
 import aiohttp
 import asyncio
 import zipfile
-import tempfile
 import shutil
+import click
+
 
 class Opus:
     def __init__(self):
@@ -15,9 +17,11 @@ class Opus:
         self.tgt = None
 
     def get(self, src: str, tgt: str, max_corpus: int = 10):
-        return asyncio.run(self.async_get('en', 'ab', max_corpus))
+        print('get...')
+        return asyncio.run(self.async_get(src, tgt, max_corpus))
 
-    def download(self, path:str, max_sentences:int = 100_000):
+    def download(self, path:str, max_sentences:int = 20_000_000):
+        print('download...')
         return asyncio.run(self.async_download(path, max_sentences))
 
     async def async_get(self, src: str, tgt: str, max_corpus: int = 10):
@@ -34,6 +38,7 @@ class Opus:
                             break
 
                         corpus = el.td.b.get_text()
+                        
                         async with session.get(f'https://opus.nlpl.eu/opusapi/?corpus={corpus}&source={src}&target={tgt}&preprocessing=moses&version=latest') as resp:
                             rep = await resp.json()
 
@@ -64,6 +69,7 @@ class Opus:
             async with aiohttp.ClientSession() as session:
                 async with session.get(self.output[v]['links'], timeout=None) as resp:
                     if resp.status == 200:
+                        print('download ', v)
                         while True:
                             chunk = await resp.content.read(16144)
                             if not chunk:
@@ -73,7 +79,7 @@ class Opus:
                             await f.write(chunk)
                             
                             await f.close()
-            
+                        print('completed')
             total_sentences += self.output[v]['sentences']
 
             for f in await aiofiles.os.listdir(dirpath):
@@ -81,8 +87,8 @@ class Opus:
                     zip_ref.extractall(dirpath)
                 
 
-                shutil.move(f"{dirpath}/{f.split('.')[0]}.{self.tgt}-{self.src}.{self.src}", f"{path}/{f.split('.')[0]}-{self.src}.txt")
-                shutil.move(f"{dirpath}/{f.split('.')[0]}.{self.tgt}-{self.src}.{self.tgt}", f"{path}/{f.split('.')[0]}-{self.tgt}.txt")
+                shutil.move(f"{dirpath}/{f.split('.')[0]}.{self.tgt}-{self.src}.{self.src}", f"{path}{f.split('.')[0]}-{self.src}.txt")
+                shutil.move(f"{dirpath}/{f.split('.')[0]}.{self.tgt}-{self.src}.{self.tgt}", f"{path}{f.split('.')[0]}-{self.tgt}.txt")
 
                 output[f.split('.')[0]] = {
                     'src': f"{f.split('.')[0]}-{self.src}.txt",
@@ -90,3 +96,33 @@ class Opus:
                 }
 
         return output
+
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    ctx.obj = Opus()
+
+@click.command()
+@click.argument('src')
+@click.argument('tgt')
+@click.option('--max_corpus', type=int, default=10)
+@click.pass_obj
+def get(ctx, src: str, tgt: str, max_corpus: int = 10):
+    print(ctx.get(src, tgt, max_corpus))
+
+@click.command()
+@click.argument('src')
+@click.argument('tgt')
+@click.argument('path')
+@click.option('--max_corpus', type=int, default=10)
+@click.option('--max_sentences', type=int, default=20_000_000)
+@click.pass_obj
+def download(ctx, src: str, tgt: str, path: str, max_sentences: int = 20_000_000, max_corpus: int = 10):
+    ctx.get(src, tgt, max_corpus)
+    print(ctx.download(path, max_sentences))
+
+if __name__ == "__main__":
+    cli.add_command(get)
+    cli.add_command(download)
+    cli()
--- readme.md
+++ readme.md
@@ -2,9 +2,27 @@
 
 Download aligned text automatly.
 
+# Installation
+
+1) Download from github: `git clone https://github.com/Interaction-Bot/opus-nlp-downloader.git`
+2) Go to the directory with `cd opus-nlp-downloader`
+3) Install dependency with `pip install -r requirements.txt`
+
 # Usage
 
+## Cli
+
+Get datasets: `python main.py get src tgt (optional: --max_corpus)`.
+exemple: `python main.py get en fr`
+
+Download datasets: `python main.py download src tgt path (optional: --max_corpus) (optional: --max_sentences)`.
+exemple: `python main.py download en fr data/`
+
+## Python
+
 ```py
+from main import *
+
 opus = Opus()
 opus.get('en', 'ab')
 opus.download('data')
